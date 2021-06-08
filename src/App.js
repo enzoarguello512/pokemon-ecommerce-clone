f6d46090a51f43de5ffbecf9759740ccf2b05e2a
Enzo Arguello <xlsones003@gmail.com>
2021-06-08 19:41:01 -0300

    fixed typing errors, deleted scss folder, css classes moved to index.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 //import logo from './logo.svg';
 import Navbar from './components/Navbar/Navbar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -3,7 +3,7 @@ import React, {useState} from "react"
 function CartWidget() {
   return (
     <li className="nav-item ms-auto d-flex align-items-center">
-      <i class="fas fa-shopping-cart fa-fw fs-4 h-pointer"></i>
+      <i className="fas fa-shopping-cart fa-fw fs-4 h-pointer"></i>
     </li>
   )
 }
