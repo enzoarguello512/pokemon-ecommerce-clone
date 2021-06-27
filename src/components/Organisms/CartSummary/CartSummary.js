c3c67c1e6792cbb9ef74d0379fcdacf1b81b9f17
Enzo Arguello <xlsones003@gmail.com>
2021-06-27 19:34:39 -0300

    the design of the items added to the shopping cart was updated

--- src/components/Organisms/CartSummary/CartSummary.js
+++ src/components/Organisms/CartSummary/CartSummary.js
@@ -16,13 +16,13 @@ function CartSummary() {
           <h3 className="text-center m-auto">The Cart is empty.</h3>
         </div>
 
-        <div className="shadow rounded row my-2">
-          <div className="col-2 bg-primary text-center rounded-start">
+        <div className="shadow rounded row my-3">
+          <div className="col-2 text-center rounded-start">
             <figure className="m-0">
               <Image imgClass="img-fluid altura-2 my-2" src="/images/BWP_ES_BW41.png" alt="test" />
             </figure>
           </div>
-          <div className="col-4 bg-info">
+          <div className="col-4">
             <div className="d-flex flex-column justify-content-center h-100">
               <h3 className="fw-bold text-truncate">Item title</h3>
               <p className="text-truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione et, maxime tempore tenetur eum in neque incidunt ducimus ex dicta ea autem reiciendis delectus magni natus. Sed laborum maxime ab.</p>
@@ -30,21 +30,21 @@ function CartSummary() {
           </div>
           <div className="col-3">
             <div className="d-flex flex-column justify-content-center h-100">
-              <label htmlFor="test">Cantidad:</label>
-              <div className="input-group">
-                <Button>-</Button>
-                <Input inputClass="text-center w-100px" id="test" ariaLabel="item quantity" />
-                <Button>+</Button>
+              <label htmlFor="test" className="text-center">Cantidad:</label>
+              <div className="input-group justify-content-center">
+                <Button btnClass="btn-red text-white border">-</Button>
+                <Input inputClass="text-center w-100px border" id="test" ariaLabel="item quantity" value="1" />
+                <Button btnClass="btn-red text-white border">+</Button>
               </div>
             </div>
           </div>
-          <div className="col-3 bg-success rounded-end">
+          <div className="col-3 rounded-end">
             <div className="text-end">
-              <Button btnClass="btn-light text-danger shadow my-2">
+              <Button btnClass="btn-white text-danger border my-2">
                 <i class="far fa-trash-alt"></i>
               </Button>
+              <div className="fw-bold mt-2">245.00 $</div>
             </div>
-            <div className="fw-bold">245.00 $</div>
           </div>
         </div>
 
