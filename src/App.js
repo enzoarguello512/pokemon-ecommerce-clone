f8d6075c5e42398518dcfe4460de553f7198ba4c
Enzo Arguello <xlsones003@gmail.com>
2021-07-26 15:05:36 -0300

    test fix deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 //COMPONENTS
